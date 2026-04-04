82e85e384c44b4620ede0f161be2e5273493b322
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2026-04-04 13:17:19 +0600

    Update compileSdk and targetSdk to 35; upgrade Android Gradle plugin to 8.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "me.ibrahimrafi.wififileshare"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.ibrahimrafi.wififileshare"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
